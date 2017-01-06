9ba14aa502180a82d6f596137542b5b3b21fbc79
Blake Knight <oss.ideas@gmail.com>
2017-01-05 21:34:25 -0600

    🤑 first working version

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const emoj = require('emoj')
+const got = require('got')
 
-module.exports = () => emoj
+module.exports = () => str => got('emoji.getdango.com/api/emoji', {
+  json: true,
+  query: {
+    q: str
+  }
+}).then(res => res.body.results.map(x => ({
+  icon: 'fa-calculator',
+  title: x.text,
+  subtitle: 'Select item to copy the value to the clipboard.',
+  value: x.text
+})))

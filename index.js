45694ede9169b57769a545e3fe3955902e7e129a
Blake Knight <oss.ideas@gmail.com>
2017-01-07 14:24:08 -0600

    ✏️ change icon in results; shorten text

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = () => str => got('emoji.getdango.com/api/emoji', {
     q: str
   }
 }).then(res => res.body.results.map(x => ({
-  icon: 'fa-calculator',
+  icon: 'fa-smile-o',
   title: x.text,
-  subtitle: 'Select item to copy the value to the clipboard.',
+  subtitle: 'Select to copy to the clipboard',
   value: x.text
 })))
